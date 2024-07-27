42a29054975c213f037bb3b8740fdac5f0257956
marti <martin.steinmassl5@gmail.com>
2024-07-27 11:13:49 +0200

    Moin

--- RadMangerForm/Model/MainModel.cs
+++ RadMangerForm/Model/MainModel.cs
@@ -38,6 +38,48 @@ namespace RadMangerForm.Model
                     int rowsAffected = command.ExecuteNonQuery();                                   // Executes and returns how many rows are effected
                     Console.WriteLine($"{rowsAffected} rows were deleted.");
                 }
+           
+            }
+        }
+        public static void LoadData()
+        {
+            string connectionString = "server=localhost;user id=root;password=;database=radmanager";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand command = connection.CreateCommand();
+
+                // Abfrage für alle Tabellen
+                string[] tables = { "Bundesland", "Person", "Fahrrad", "Strecken", "Belag", "Koordinaten", "Trinkbrunnen", "ZOT_Strecke_Trinkbrunnen" };
+
+                foreach (string table in tables)
+                {
+                    command.CommandText = $"SELECT * FROM {table}";
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine($"Table: {table}");
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader.GetName(i) + "\t");
+                        }
+                        Console.WriteLine();
+
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.Write(reader[i] + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine();
+                    }
+                }
+
+                // Verbindung schließen
+                connection.Close();
             }
         }
     }
